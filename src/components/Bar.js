5840e456676d6e6cddfff6155c6c8cabd2e99d15
Avinash Sharma <avinashsharma8199@gmail.com>
2021-08-29 22:58:58 +0530

    first time

--- src/components/Bar.js
+++ src/components/Bar.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import "./Bar.css"
 
-function Bar({ length, colorKey }) {
-  const COLOR_SET = ["grey", "orange", "green"];
+function Bar({
+    length,
+    colorKey
+}) {
+    const COLOR_SET = ["grey", "red", "green"];
 
-  let color = COLOR_SET[colorKey];
+    let color = COLOR_SET[colorKey];
 
-  let style = {
-    height: length,
-    backgroundColor: color,
-  }
+    let style = {
+        height: length,
+        backgroundColor: color,
+    }
 
-  return (
-    <div className="bar" style={style} ></div>
-  )
+    return ( < div className = "bar"
+        style = {
+            style
+        } > < /div>
+    )
 }
 
 export default Bar;
